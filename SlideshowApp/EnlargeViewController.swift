287f036c96afbea19fc887b8d27d08eea1ccfa21
marino <ymmh.cs2016@gmail.com>
2016-08-08 11:31:36 +0900

    スライドショーアプリの課題提出

---
+++ SlideshowApp/EnlargeViewController.swift
@@ -0,0 +1,43 @@
+//
+//  EnlargeViewController.swift
+//  SlideshowApp
+//
+//  Created by 佐藤まりの on H28/08/04.
+//  Copyright © 平成28年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class EnlargeViewController: UIViewController {
+    
+    
+    @IBOutlet weak var bigphoto: UIImageView!
+
+    var image:UIImage? = nil
+   
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        bigphoto.image = image
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*/Users/satoumarino/Desktop/animalimage/alpaca.jpg
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -6,20 +6,132 @@
 //  Copyright © 平成28年 mycompany. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //メソッドとプロパティの設定
+   
+    @IBOutlet weak var imagephoto: UIImageView!
+    @IBOutlet weak var back: UIButton!
+    @IBOutlet weak var startstop: UIButton!
+    @IBOutlet weak var precede: UIButton!
+
+    
+    //一時的に写真の保存
+    let photos = [UIImage(named:"alpaca.jpg"),UIImage(named:"bear.jpg"), UIImage(named:"rabbit.jpg"), UIImage(named:"koala.jpg"), UIImage(named:"giraffe.jpg")]
+    
+    //変数の宣言
+    var timer:NSTimer = NSTimer()
+    var count = 0
+    
+    
+    
+    //進む機能の設定
+    @IBAction func precede(sender: AnyObject) {
+        
+        count += 1
+        if count == 5 {
+            count = 0
+
+        }
+        imagephoto.image = photos[count]
+        
+    }
+    
+    //戻る機能の設定
+    @IBAction func back(sender: AnyObject) {
+        
+        count -= 1
+        if count == -1{
+            count = 4
+        }
+        imagephoto.image = photos[count]
+        
+
+    }
+    
+    //ボタンの再生と一時停止の機能の設定
+    @IBAction func startstop(sender: AnyObject){
+        
+        //タイマーの設定
+        if !timer.valid{
+        timer = NSTimer.scheduledTimerWithTimeInterval(2.0,
+                                                        target: self,
+                                                        selector: #selector(ViewController.result),
+                                                        userInfo: nil,
+                                                        repeats: true)
+            
+            // タイマーが動いている時：進むボタンと戻るボタンを使えないようにする
+            precede.hidden = true
+            back.hidden = true
+            
+            startstop.setTitle("■", forState: UIControlState.Normal)
+        }else {
+                
+                //タイマーが動いていない時：進むボタンと戻るボタンを使えるようにする
+                timer.invalidate()
+                
+                precede.hidden = false
+                back.hidden = false
+                
+                startstop.setTitle("▶︎", forState: UIControlState.Normal)
+
+
+            
+        }
+    }
+    
+    func result() {
+    count += 1
+        if count == 5 {
+            count = 0
+        }
+        imagephoto.image = photos[count]
+        
+           }
+    
+    //画面の遷移
+    @IBAction func imagephoto(sender: AnyObject) {
+    }
+
+    
+    
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+
+            }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // タップイベント.
+    func tapped(sender: UITapGestureRecognizer){
+        
+        // ViewController2 へ遷移するために Segue を呼び出す
+        performSegueWithIdentifier("toEnlargeViewController",sender: nil)
+        print(sender)
+    }
 
+    
+    //enlargeControllerViewの内容
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
+        let enlargeViewController:EnlargeViewController = segue.destinationViewController as! EnlargeViewController
+        
+        enlargeViewController.image = photos[count]
 
+    }
+    
+        //遷移先から遷移元へ戻る
+        @IBAction func unwind(segue: UIStoryboardSegue){
+    }
+    
 }
 
